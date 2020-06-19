e3779cc86670781080c0d1afbb01783fd5c89c3c
Ridwan <ridwanishola97@gmail.com>
2020-06-19 23:01:53 +0100

    init (restart)

--- dev/js/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import UserReducer from "./reducer-users"
-
-const allReducers = combineReducers({
-    users: UserReducer
-});
-
-export default allReducers;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,14 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require("path");
 
 module.exports = {
-    devServer: {
-        inline: true,
-        contentBase: './src',
-        port: 3000
-    },
-    devtool: 'cheap-module-eval-source-map',
-    entry: './dev/js/index.js',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss/,
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
-    output: {
-        path: 'src',
-        filename: 'js/bundle.min.js'
-    },
-    plugins: [
-        new webpack.optimize.OccurrenceOrderPlugin()
-    ]
+  entry: "./src/index.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000
+  },
+  mode: "development"
 };
