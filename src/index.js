156a825a75ddfb887b332a3c6520ee03910d7a8b
Ridwan <ridwanishola97@gmail.com>
2020-06-19 23:22:50 +0100

    tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+let input = "   Javascript   ";
+let output = "div" + input.trim + "</div>";
+
+const trim = str => str.trim();
+const wrap = (type, str) => `<${type}>${str}</${type}>`
+const toLowerCase = str => str.toLowerCase();
+
+
+const transform = pipe(trim, toLowerCase, wrap);
+console.log(transform(input));
