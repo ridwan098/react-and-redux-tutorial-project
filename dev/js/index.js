2ac1605ec8de6f6c25ede074c2bd999c6c0a8ebf
Ridwan <ridwanishola97@gmail.com>
2020-06-23 15:21:04 +0100

    list of users

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,8 +1,12 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import { createStore } from 'redux';
+
+
+const store = createStore();
 
 ReactDOM.render(
-    <h1>Hey now</h1>,
+    <h1>Hey now bitch ass</h1>,
     document.getElementById('root')
 );
