5e89bd57cd9c3cb77f520e5c019810940e64cc37
Ridwan <ridwanishola97@gmail.com>
2020-06-23 15:48:14 +0100

    making containers

--- dev/js/components/app.js
+++ dev/js/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import UserList from '../containers/user-list'
 
 
 const App = () => (
     <div>
         <h2>Username List:</h2>
+        <UserList></UserList>
         <hr />
         <h2>User Details</h2>
     </div>
